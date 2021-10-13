b4f37b5e8cbf73a9a97aff6347bffee3ee8f556c
Hernan <hernan.ss@live.com>
2021-10-13 18:21:40 -0300

    1

--- src/Components/FormularioOrden.js
+++ src/Components/FormularioOrden.js
@@ -1,90 +1,89 @@
 import getStore from '../ReactFireBase';
 import { useHistory } from 'react-router';
-import {useState} from 'react';
+import { useState } from 'react';
 
 import { useCartContext } from "../Contexto/CartContext";
 
 
 
 const FormularioOrden = () => {
-    const { carrito, precioTotal, setOrden, setCarrito,orden} = useCartContext();
-  const [nombre, setNombre] = useState ("")
-  const [telefono, setTelefono] = useState (0)
-  const [email, setEmail] = useState ("")
-  const history= useHistory()
+  const { carrito, precioTotal, setOrden, setCarrito, orden } = useCartContext();
+  const [nombre, setNombre] = useState("")
+  const [telefono, setTelefono] = useState(0)
+  const [email, setEmail] = useState("")
+  const history = useHistory()
 
-  const newOrden ={
-    buyer : {
-     nombre: nombre,
-     telefono: telefono,
-     email: email
+  const newOrden = {
+    buyer: {
+      nombre: nombre,
+      telefono: telefono,
+      email: email
     },
-   
+
     items: carrito,
-    date: new Date().toLocaleDateString ('en-GB'),
-    total: precioTotal ()
+    date: new Date().toLocaleDateString('en-GB'),
+    total: precioTotal()
   }
 
 
 
- const guardarOrden = (e) => {
-   e.preventDefault()
- 
-   const firestore = getStore()
-   const collection = firestore.collection("Ordenes")
-   collection.add (newOrden).then((orden) =>{
-    setOrden (orden.id) 
-    setCarrito ([]);
-    history.push ("/graciasporsucompra/")
-   })
+  const guardarOrden = (e) => {
+    e.preventDefault()
 
-}                  
-   
+    const firestore = getStore()
+    const collection = firestore.collection("Ordenes")
+    collection.add(newOrden).then((orden) => {
+      setOrden(orden.id)
+      setCarrito([]);
+      history.push("/graciasporsucompra/")
+    })
 
-    const handleNombre = (e) => {
-        setNombre (e.target.value) 
-        
-           }
-        
-    const handleTelefono = (e) => {
-            setTelefono (e.target.value) 
-            
-        
-           }
-            
-    const handleEmail = (e) => {
-                setEmail (e.target.value) 
-                
-                
-            }
-               
-            return ( 
-                <div style={{paddingTop:"20px"}}>
-                  <form className="form" onSubmit={guardarOrden}>
+  }
 
-                  <h4>Ingrese sus datos para realizar compra </h4>
-                <div><h4> El número de su orden es:{orden}</h4></div> 
-                <div><h5>Precio Total: $ {precioTotal()}</h5></div>
-               
-                <div> 
-                  <label className="label">Nombre y Apellido</label>
-                  <input  className="form-input" type="text" placeholder="Carlos García"id="nombre"  onChange={handleNombre} required={true}/>
-                </div>
-                <div> 
-                  <label className="label">Teléfono</label>
-                  <input  className="form-input"  placeholder="341236598" type="number" onInput={(e) => e.target.value = e.target.value.slice(0, 10)} id="telefono"onChange={handleTelefono} required={true}/>
-                </div>
-                <div>
-                  <label className="label">Email</label>
-                  <input  className="form-input" placeholder="carlosgarcia@hotmail.com"type="email" id="email" onChange={handleEmail} required={true}/>
-                  </div>
-              <button className="comprarbtn" type="submit">Finalizar Compra</button>
-                  </form>
-                  
-                    
-                </div>
-        
-            )
+
+  const handleNombre = (e) => {
+    setNombre(e.target.value)
+
+  }
+
+  const handleTelefono = (e) => {
+    setTelefono(e.target.value)
+
+  }
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+
+
+  }
+
+  return (
+    <div style={{ paddingTop: "20px" }}>
+      <form className="form" onSubmit={guardarOrden}>
+
+        <h4>Ingrese sus datos para realizar compra </h4>
+
+        <div><h5>Precio Total: $ {precioTotal()}</h5></div>
+
+        <div>
+          <label className="label">Nombre y Apellido</label>
+          <input className="form-input" type="text" placeholder="Carlos García" id="nombre" onChange={handleNombre} required={true} />
+        </div>
+        <div>
+          <label className="label">Teléfono</label>
+          <input className="form-input" placeholder="341236598" type="number" onInput={(e) => e.target.value = e.target.value.slice(0, 10)} id="telefono" onChange={handleTelefono} required={true} />
+        </div>
+        <div>
+          <label className="label">Email</label>
+          <input className="form-input" placeholder="carlosgarcia@hotmail.com" type="email" id="email" onChange={handleEmail} required={true} />
+        </div>
+        <button className="comprarbtn" type="submit">Finalizar Compra</button>
+      </form>
+
+
+    </div>
+
+  )
 
 
 }
--- src/Components/GraciasPorSuCompra.js
+++ src/Components/GraciasPorSuCompra.js
@@ -1,17 +1,19 @@
 import graciasPorSuCompra from "../../src/img/atelier3.jpg"
+import { useCartContext } from "../Contexto/CartContext";
 const GraciasPorSuCompra = () => {
 
-
+    const { orden } = useCartContext();
     return (
         <div>
-     <h2>Muchas gracias por su compra!</h2>
-     <div>
-         <img src= {graciasPorSuCompra} alt="" className="imgGraciasXCompra" />
-    </div>
+            <h2>Muchas gracias por su compra!</h2>
+            <h4>Su numero de orden: {orden}</h4>
+            <div className="imgGraciasXCompra">
+                <img src={graciasPorSuCompra} alt="" />
+            </div>
         </div>
-     
-    
-     
+
+
+
     )
 }
 
